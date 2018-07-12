df643bb40d8c9618526df4fd3097911673262e3b
stophobia <stophobia@gmail.com>
2018-07-12 18:35:57 +0900

    first commit

---
+++ app.ts
@@ -0,0 +1,10 @@
+import { ServerAPI } from './serverModule'
+ 
+class Main {
+    constructor() {
+        const serverAPI = new ServerAPI()
+        serverAPI.initServer()
+    }
+}
+ 
+const main = new Main()
